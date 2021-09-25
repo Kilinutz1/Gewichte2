98be2cded393447f44a30a3789122ca200317428
andi <andi.muehlmann@arcor.de>
2021-09-25 14:16:09 +0200

    first commit

--- main.py
+++ main.py
@@ -1,4 +1,5 @@
 import requests
+from collections import deque
 
 def get_weigts():
     url = 'https://bwinf.de/fileadmin/user_upload/gewichtsstuecke0.txt'
@@ -6,11 +7,40 @@ def get_weigts():
     doc = result.content.decode("utf-8").split()
     weights = []
     for i in range(1, len(doc), 2):
-        weights.append([doc[i], doc[i + 1]])
+        weights.append([int(doc[i]), int(doc[i + 1])])
     return weights
+
+def give_possible_combination(combination1, combination2):
+    pass
+
+def find_combination(weights, searched_weight, combination=deque([]), deleted_weights=0):
+    if searched_weight == 0:
+        return combination
+    for weight in range(len(weights)):
+        weight -= deleted_weights
+        if weights[weight][1] == 0:
+            del weights[weight]
+            deleted_weights += 1
+            continue 
+        weight[1] -= 1
+        combination1 = find_combination(weights, searched_weight + weights[weight][0], combination)
+        combination2 = find_combination(weights, searched_weight - weights[weight][1], combination)
+        combination = give_possible_combination(combination1, combination2)
+        if combination:
+            return combination
+    return False
+
+def print_combination_for_weight(weight, combination):
+    pass
 
 def main():
     weights = get_weigts()
+    for weight in range(10,10000, 10):
+        combination = find_combination(weights, weight)
+        if combination:
+            print_combination_for_weight(weight, combination)
+        else:
+            print(f'There is no combination for {weight}g')
     return
 
 if __name__ == '__main__':
